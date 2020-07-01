7aef5cf5e52e8472645d66a4f47c613cb53c47a3
ZhaoWenming <1163556811@qq.com>
2020-07-01 18:44:54 +0800

    新增

--- springboot/springboot-quartz/src/main/java/org/zwm/quartz/SpringbootQuartzApplication.java
+++ springboot/springboot-quartz/src/main/java/org/zwm/quartz/SpringbootQuartzApplication.java
@@ -1,13 +1,17 @@
 package org.zwm.quartz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootQuartzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootQuartzApplication.class, args);
+        log.info("{}springboot quartz startup success{}", "=======", "=======");
+
     }
 
 }
