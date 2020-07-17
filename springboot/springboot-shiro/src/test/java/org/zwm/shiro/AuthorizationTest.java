a6b9dcf1bd2efe3bb54a23ce8bccaea1c91c1374
ZhaoWenming <1163556811@qq.com>
2020-07-17 09:01:20 +0800

    新增

---
+++ springboot/springboot-shiro/src/test/java/org/zwm/shiro/AuthorizationTest.java
@@ -0,0 +1,15 @@
+package org.zwm.shiro;
+
+/**
+ * 访问控制测试：在应用中控制谁能访问哪些资源
+ * 几个关键对象：主体（Subject）、资源（Resource）、权限（Permission）、角色（Role）
+ * 主体：...
+ * 资源：...
+ * 权限：表示在应用中用户能不能访问某个资源
+ * 角色：代表了操作集合，可以理解为权限的集合
+ * 隐式角色：
+ * 显示角色：
+ */
+public class AuthorizationTest {
+
+}
