87d0131a921f2fa25c1288da810ab2d60918027b
ZhaoWenming <1163556811@qq.com>
2020-08-03 17:29:56 +0800

    修改/新增

--- disign-patterns/src/main/java/org/zwm/factory/method/Cosumer.java
+++ disign-patterns/src/main/java/org/zwm/factory/method/Cosumer.java
@@ -4,9 +4,7 @@ public class Cosumer {
     public static void main(String[] args) {
         Car car = new WuLingFactory().getCar();
         Car car1 = new TeslaFactory().getCar();
-        Car car2 = new MoBaiFactory().getCar();
         car.name();
         car1.name();
-        car2.name();
     }
 }
